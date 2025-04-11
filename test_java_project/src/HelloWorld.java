b5b2806ce7e3709e1fd440108731ec67a0424608
it <it@DESKTOP-LR7JI23>
2025-04-11 14:33:46 +0900

    첫 커밋!

--- test_java_project/src/HelloWorld.java
+++ test_java_project/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 
 	public static void main(String[] args) 
 	{
-          System.out.println("안녕 Hello World!!!!~~");
+          System.out.println("안녕 Hello World!!!!!~~");
 	}
 
 }
